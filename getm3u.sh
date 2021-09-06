ab1a2acebc0f4e34186416787215d1683630df6b
qeeai <shaun.h.qiu@gmail.com>
2021-09-07 05:41:20 +0800

    CCTV Renew!

--- getm3u.sh
+++ getm3u.sh
@@ -2,7 +2,7 @@
 rm -rf index.m3u
 touch index.m3u
 curl https://iptv-org.github.io/iptv/countries/cn.m3u | sed 's/group-title=".*"/group-title="中国"/g'  >> index.m3u
-cat  hk.m3u | sed 's/group-title=".*"/group-title="香港"/g'  >> index.m3u
+cat  hk.m3u >> index.m3u
 curl https://iptv-org.github.io/iptv/countries/tw.m3u | sed 's/group-title=".*"/group-title="台湾"/g'  >> index.m3u
 curl https://iptv-org.github.io/iptv/countries/jp.m3u | sed 's/group-title=".*"/group-title="日本"/g'  >> index.m3u
 curl https://iptv-org.github.io/iptv/countries/kr.m3u | sed 's/group-title=".*"/group-title="韩国"/g'  >> index.m3u
@@ -12,3 +12,4 @@ curl https://iptv-org.github.io/iptv/countries/fr.m3u | sed 's/group-title=".*"/
 curl https://iptv-org.github.io/iptv/countries/de.m3u | sed 's/group-title=".*"/group-title="德国"/g'  >> index.m3u
 curl https://iptv-org.github.io/iptv/countries/th.m3u | sed 's/group-title=".*"/group-title="泰国"/g'  >> index.m3u
 cp -rf index.m3u ../qeeai.github.io/iptv/
+rm -rf index.m3u
